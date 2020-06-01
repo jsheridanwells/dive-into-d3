f288e1393d00bbfee0423aeded6cd38304b8d36b
Jeremy Wells <jsheridanwells@gmail.com>
2020-06-01 15:03:44 -0500

    fixed the way d3 is loaded

--- javascripts/line-chart.js
+++ javascripts/line-chart.js
@@ -1,12 +1,5 @@
 'use strict';
 
-let d3;
-
-import('https://d3js.org/d3.v5.min.js').then(res => {
-  debugger;
-  d3 = res
-});
-
 // borrowed heavily from this chart:
 // https://bl.ocks.org/pstuffa/26363646c478b2028d36e7274cedefa6
 
